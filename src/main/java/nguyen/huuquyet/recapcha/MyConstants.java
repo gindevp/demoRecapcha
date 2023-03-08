5899a45209a580a65e45aef1ab53d77d2a0e5ac9
quyet <=>
2023-03-08 20:41:12 +0700

    done

---
+++ src/main/java/nguyen/huuquyet/recapcha/MyConstants.java
@@ -0,0 +1,6 @@
+package nguyen.huuquyet.recapcha;
+
+public class MyConstants {
+	public static final String SITE_KEY ="6LdVMeMkAAAAANnm4EDrvn4JVU1TBiWfjooRU9-J";
+	public static final String SECRET_KEY ="6LdVMeMkAAAAAKpXUv76WnzU2fx6HkacL9A7X4_K";
+}
